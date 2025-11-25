0e5409ca7d648e7f9115c56564ae45923c1012f7
wade-ary <161520052+wade-ary@users.noreply.github.com>
2025-11-25 00:01:46 -0500

    Added api calls from 4 sources

--- news_api/api_calls.py
+++ news_api/api_calls.py
@@ -1,4 +1,8 @@
 from eventregistry import EventRegistry, QueryArticlesIter, QueryItems
+from newsdataapi import NewsDataAPIClient
+from finlight_client import FinlightApi
+from finlight_client.models import GetArticlesParams
+import http.client, urllib.parse
 import json
 
 # api details https://www.newsapi.ai/intro-python
@@ -27,4 +31,90 @@ def fetch_event_registry(keyword: str, max_items: int = 10):
             "body": art.get("body")
         })
 
+    return results
+
+# api details https://newsdata.io/blog/news-api-python-client/
+def fetch_news_data(keyword: str, max_items: int = 10):
+    """
+    Run a single-query search in news data api for a given keyword.
+    Returns a list of standardized article dictionaries.
+    """
+    api = NewsDataApiClient(apikey='YOUR_API_KEY')
+
+    response = api.latest_api(q=keyword, max_results = max_items)
+    results = []
+
+    for art in response:
+        results.append({
+            "title": art.get("title"),
+            "url": art.get("link"),
+            "source": art.get("source_name"),
+            "published_at": art.get("pubDate"),
+            "body": art.get("body")
+        })
+    return results
+
+# api details at https://docs.finlight.me/v2/rest-endpoints/
+def fetch_finflight(keyword: str, max_items: int = 10):
+    """
+    Run a single-query search in fin flight api for a given keyword.
+    Returns a list of standardized article dictionaries.
+    """
+
+    client = FinlightApi(
+    config=ApiConfig(
+        api_key="your_api_key"
+    )
+    )       
+
+    params = GetArticlesParams(
+        query=keyword,
+ 
+    )
+
+    response = client.articles.fetch_articles(params=params)
+    results = []
+
+    for art in response.get("articles"):
+        results.append({
+            "title": art.get("title"),
+            "url": art.get("link"),
+            "source": art.get("source_name"),
+            "published_at": art.get("publishDate"),
+            "body": art.get("summary")
+        })
+    
+    return results
+
+def fetch_the_news_api(keyword : str, max_items: int = 10):
+    """
+    Run a single-query search in fin flight api for a given keyword.
+    Returns a list of standardized article dictionaries.
+    """
+    conn = http.client.HTTPSConnection('api.thenewsapi.com')
+
+    params = urllib.parse.urlencode({
+        'api_token': 'YOUR_API_TOKEN',
+        'categories': 'business,tech',
+        'limit': 50,
+        })
+
+    conn.request('GET', '/v1/news/all?{}'.format(params))
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print(data.decode('utf-8'))
+
+    results = []
+    
+    for art in data:
+        results.append({
+            "title": art.get("title"),
+            "url": art.get("url"),
+            "source": art.get("source"),
+            "published_at": art.get("published_at"),
+            "body": art.get("description")
+        })
+    
     return results
