bccdd2ceba2a1779d90f03cdf6fb320da59fa797
wade-ary <161520052+wade-ary@users.noreply.github.com>
2025-11-29 12:00:00 -0500

    Clustering and ready for last stage retrival remains

--- graph/graph_analysis.py
+++ graph/graph_analysis.py
@@ -1,6 +1,6 @@
 from igraph import Graph
 import leidenalg
-
+from openai import OpenAI
 
 def run_community(G):
     partition = leidenalg.find_partition(G, leidenalg.RBConfigurationVertexPartition, weights='weight')
@@ -8,3 +8,47 @@ def run_community(G):
     return partition
 
 
+def analyze_clusters(articles, partition):
+    clusters = {}
+
+    for cid, node_ids in enumerate(partition):
+        # Gather raw text of all articles in the cluster
+        cluster_texts = [articles[i]["text"] for i in node_ids]
+        full_text = "\n".join(cluster_texts)
+
+        keywords = []
+        for i in node_ids:
+            keywords.extend(articles[i]["keywords"])
+    
+        embedding = []
+        for i in node_ids:
+            embedding.append(articles[i]["embedding"])
+        combined_summary = summarize_cluster(full_text)
+
+        clusters[cid] = {
+            "article_ids": node_ids,
+            "keywords": keywords,
+            "embedding": embedding,
+            "combined_summary": combined_summary
+        }
+
+    return clusters
+
+client = OpenAI()
+def summarize_cluster(text):
+    prompt = f"""
+Give a proper summary of this combinations of articles they talk about a similar topic point out
+similarities and differences.
+
+topics:
+{text}
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": prompt.strip()}
+        ]
+    )
+
+    return response.choices[0].message.content
